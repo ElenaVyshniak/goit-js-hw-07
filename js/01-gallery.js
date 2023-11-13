489e750cc98ada88699150eefd72bcbc379979e6
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-13 15:25:27 +0200

    jsfinish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,3 @@
-// import { galleryItems } from "./gallery-items.js";
-// // Change code below this line
-// const galleryContainer = document.querySelector(`.gallery`);
-// const galleryMarkup = createGalleryItemsMarkup(galleryItems);
-
-// galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
-
-// galleryContainer.addEventListener("click", tapGelleryItem);
-
-// function createGalleryItemsMarkup(galleryItems) {
-//   return galleryItems
-//     .map((item) => {
-//       // предполагаем, что item имеет свойства, например, src и alt для изображений
-//       return `<li class="gallery_item">
-//         <a class="gallery_link" href="${item.original}">
-//             <img class="gallery_image"  src="${item.preview}"  alt="${item.description}" width="340px"
-//             </a>
-//     </li>`;
-//     })
-//     .join("");
-// }
-// function tapGelleryItem(event) {
-//   event.preventDefault();
-//   if (!event.target.classList.contains("gallery_image")) {
-//     return;
-//   }
-//   console.log(event.target);
-// }
-
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
