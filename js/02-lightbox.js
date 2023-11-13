e115e0b7d31e1bd0d623ad22f9dfe3fa26886483
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-13 16:22:43 +0200

    js02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,41 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const container = document.querySelector(".gallery");
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `
+    )
+    .join("");
+}
+
+container.addEventListener("click", handleClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  lightbox.open(1);
+}
